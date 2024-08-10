c5817b8dcabda8b438db0ab1594045fc3f8eefc6
Your Name <you@example.com>
2024-08-10 20:56:47 +0800

    1

---
+++ c267893a-f793-4740-86b4-7bb50308e678.ts
@@ -0,0 +1,61 @@
+export default {
+  "entity": {
+    "id": "c267893a-f793-4740-86b4-7bb50308e678",
+    "name": "抓纲治豫 大干快上 迅速赶上全国前进的步伐——段君毅同志在省委常委扩大会议上的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "段君毅",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/13.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/14.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/15.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/16.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/17.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/18.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/19.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/20.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/c267893a-f793-4740-86b4-7bb50308e678/21.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "抓纲治豫 大干快上 迅速赶上全国前进的步伐——段君毅同志在省委常委扩大会议上的讲话",
+        "authors": [
+          "段君毅"
+        ],
+        "page_start": 1,
+        "page_end": 21,
+        "dates": [
+          {
+            "year": 1978,
+            "month": 11,
+            "day": 19
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c267893a-f793-4740-86b4-7bb50308e678",
+  "resource_type": "book",
+  "version": 2
+}
