b10bb6ba24cb796601ee7f071d844ffe89f8b031
Your Name <you@example.com>
2024-08-13 20:27:57 +0800

    fix

--- 4bdccb11-dd6d-430c-9d22-1cff22789261.ts
+++ 4bdccb11-dd6d-430c-9d22-1cff22789261.ts
@@ -1,43 +1,43 @@
 export default {
-  "entity": {
-    "id": "4bdccb11-dd6d-430c-9d22-1cff22789261",
-    "name": "胡尚礼同志在省委常委扩大会议上的讲话",
-    "internal": false,
-    "type": "imgs",
-    "official": false,
-    "author": "胡尚礼",
-    "files": [
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/4bdccb11-dd6d-430c-9d22-1cff22789261/1.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/4bdccb11-dd6d-430c-9d22-1cff22789261/2.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/4bdccb11-dd6d-430c-9d22-1cff22789261/3.jpg"
-    ]
-  },
-  "parser_option": {
-    "articles": [
-      {
-        "title": "胡尚礼同志在省委常委扩大会议上的讲话",
-        "authors": [
-          "胡尚礼"
-        ],
-        "page_start": 1,
-        "page_end": 3,
-        "dates": [
-          {
-            "year": 1977,
-            "month": 10,
-            "day": 24
-          }
-        ]
-      }
+  entity: {
+    id: '4bdccb11-dd6d-430c-9d22-1cff22789261',
+    name: '胡尚礼同志在省委常委扩大会议上的讲话',
+    internal: false,
+    type: 'img',
+    official: false,
+    author: '胡尚礼',
+    files: [
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/4bdccb11-dd6d-430c-9d22-1cff22789261/1.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/4bdccb11-dd6d-430c-9d22-1cff22789261/2.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/4bdccb11-dd6d-430c-9d22-1cff22789261/3.jpg',
     ],
-    "ocr": {
-      "use_onnx": true,
-      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
-      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
-    }
   },
-  "parser_id": "automation",
-  "path": "4bdccb11-dd6d-430c-9d22-1cff22789261",
-  "resource_type": "book",
-  "version": 2
-}
+  parser_option: {
+    articles: [
+      {
+        title: '胡尚礼同志在省委常委扩大会议上的讲话',
+        authors: [
+          '胡尚礼',
+        ],
+        page_start: 1,
+        page_end: 3,
+        dates: [
+          {
+            year: 1977,
+            month: 10,
+            day: 24,
+          },
+        ],
+      },
+    ],
+    ocr: {
+      use_onnx: true,
+      det_model_dir: './paddle/onnx/ch_PP-OCRv4_det_infer.onnx',
+      rec_model_dir: './paddle/onnx/ch_PP-OCRv4_rec_infer.onnx',
+    },
+  },
+  parser_id: 'automation',
+  path: '4bdccb11-dd6d-430c-9d22-1cff22789261',
+  resource_type: 'book',
+  version: 2,
+};
--- d6f15f25-9b49-4390-a1af-ec1e50b94cda.ts
+++ d6f15f25-9b49-4390-a1af-ec1e50b94cda.ts
@@ -1,72 +1,72 @@
 export default {
-  "entity": {
-    "id": "d6f15f25-9b49-4390-a1af-ec1e50b94cda",
-    "name": "纪登奎同志在整党建党工作座谈会上的讲话",
-    "internal": false,
-    "type": "imgs",
-    "official": false,
-    "author": "纪登奎",
-    "files": [
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/1.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/2.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/3.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/4.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/5.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/6.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/7.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/8.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/9.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/10.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/11.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/12.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/13.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/14.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/15.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/16.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/17.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/18.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/19.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/20.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/21.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/22.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/23.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/24.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/25.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/26.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/27.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/28.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/29.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/30.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/31.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/32.jpg"
-    ]
-  },
-  "parser_option": {
-    "articles": [
-      {
-        "title": "纪登奎同志在整党建党工作座谈会上的讲话",
-        "authors": [
-          "纪登奎"
-        ],
-        "page_start": 1,
-        "page_end": 32,
-        "dates": [
-          {
-            "year": 1970,
-            "month": 4,
-            "day": 15
-          }
-        ]
-      }
+  entity: {
+    id: 'd6f15f25-9b49-4390-a1af-ec1e50b94cda',
+    name: '纪登奎同志在整党建党工作座谈会上的讲话',
+    internal: false,
+    type: 'img',
+    official: false,
+    author: '纪登奎',
+    files: [
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/1.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/2.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/3.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/4.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/5.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/6.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/7.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/8.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/9.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/10.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/11.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/12.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/13.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/14.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/15.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/16.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/17.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/18.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/19.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/20.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/21.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/22.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/23.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/24.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/25.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/26.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/27.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/28.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/29.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/30.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/31.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives30/main/d6f15f25-9b49-4390-a1af-ec1e50b94cda/32.jpg',
     ],
-    "ocr": {
-      "use_onnx": true,
-      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
-      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
-    }
   },
-  "parser_id": "automation",
-  "path": "d6f15f25-9b49-4390-a1af-ec1e50b94cda",
-  "resource_type": "book",
-  "version": 2
-}
+  parser_option: {
+    articles: [
+      {
+        title: '纪登奎同志在整党建党工作座谈会上的讲话',
+        authors: [
+          '纪登奎',
+        ],
+        page_start: 1,
+        page_end: 32,
+        dates: [
+          {
+            year: 1970,
+            month: 4,
+            day: 15,
+          },
+        ],
+      },
+    ],
+    ocr: {
+      use_onnx: true,
+      det_model_dir: './paddle/onnx/ch_PP-OCRv4_det_infer.onnx',
+      rec_model_dir: './paddle/onnx/ch_PP-OCRv4_rec_infer.onnx',
+    },
+  },
+  parser_id: 'automation',
+  path: 'd6f15f25-9b49-4390-a1af-ec1e50b94cda',
+  resource_type: 'book',
+  version: 2,
+};
